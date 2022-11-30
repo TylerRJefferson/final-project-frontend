4d0a6250d88b9bc3285a96ecf24222ab1d7bf776
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-30 15:18:48 -0500

    moved AppBar into separate component file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Box, Grommet, Heading, Menu } from "grommet";
-import { Menu as MenuIcon } from "grommet-icons";
+import { Box, Grommet } from "grommet";
+import AppBar from "./components/AppBar"
 import AddVehicleForm from "./components/AddVehicleForm"
 
 const theme = {
@@ -16,40 +16,15 @@ const theme = {
   },
 };
 
-const AppBar = (props) => (
-  <Box
-    tag="header"
-    direction="row"
-    align="center"
-    justify="between"
-    background="brand"
-    pad={{ left: "medium", right: "medium", vertical: "small" }}
-    elevation="medium"
-    style={{ zIndex: "1" }}
-    {...props}
-  />
-);
-
 function App() {
   return (
     <Grommet theme={theme} full>
       <Box fill>
-        <AppBar>
-          <Heading level="2" margin="none" color="black">Pocket Mechanic</Heading>
-          <Menu
-            icon={<MenuIcon color="black"/>}
-            dropBackground="light-2"
-              items={[
-                { label: 'Add New Vehicle', onClick: () => {} },
-                { label: 'My Garage', onClick: () => {} },
-                // { label: 'Log Out', onClick: () => {} },
-              ]}
-          />
-        </AppBar>
+        <AppBar />
         <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
           <Box flex align="center" justify="center">
             <AddVehicleForm />
-          </Box>     
+          </Box>
         </Box>
       </Box>
     </Grommet>
---
+++ src/components/AppBar.jsx
@@ -0,0 +1,33 @@
+import { Box, Heading, Menu } from "grommet";
+import { Menu as MenuIcon } from "grommet-icons";
+
+const MenuBar = (props) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "medium", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
+
+export default function AppBar() {
+  return (
+  <MenuBar>
+    <Heading level="2" margin="none" color="black">Pocket Mechanic</Heading>
+    <Menu
+      icon={<MenuIcon color="black" />}
+      dropBackground="light-2"
+      items={[
+        { label: 'Add New Vehicle', onClick: () => { } },
+        { label: 'My Garage', onClick: () => { } },
+        // { label: 'Log Out', onClick: () => {} },
+      ]}
+    />
+  </MenuBar>
+  )
+}
