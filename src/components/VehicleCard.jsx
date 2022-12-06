52f7c626a7b8b8ce77ef2703567e628bb99ed581
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-12-05 21:18:29 -0500

    show logs most recent first and moved buttons to top

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { Box, Button, Card, CardBody, CardFooter, Grommet } from "grommet";
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Grommet } from "grommet";
 import ReactCardFlip from "react-card-flip";
 import { useState } from "react";
 
@@ -51,8 +51,14 @@ export default function VehicleCard({ thisVehicle }) {
         
         <Box margin="10px 0px 10px 0px">
           <Card pad="medium" background="light-4">
+            <CardHeader>
+              <Button secondary size="small" label="Log Maintenance"
+                onClick={() => navigate(`/logs/${thisVehicle._id}`)} />
+              <Button secondary size="small" label="Show Vehicle"
+                onClick={() => setFlip(!flip)} />
+            </CardHeader>
             <CardBody pad="small">
-              {thisVehicle.logs.map((log) => {
+              {[...thisVehicle.logs].reverse().map((log) => {
                 return (
                   <>
                   <h3>{log.maint_type}</h3>
@@ -61,12 +67,6 @@ export default function VehicleCard({ thisVehicle }) {
                   )
               })}
             </CardBody>
-            <CardFooter>
-              <Button secondary size="small" label="Log Maintenance"
-                onClick={() => navigate(`/logs/${thisVehicle._id}`)} />
-              <Button secondary size="small" label="Show Vehicle"
-                onClick={() => setFlip(!flip)} />
-            </CardFooter>
           </Card>
         </Box>
       </ReactCardFlip>
