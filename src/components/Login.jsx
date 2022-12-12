ad725f2a14faeff548f87167aa0e6ba7da763fd3
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-12-11 22:31:13 -0500

    login page styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, 
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider,
   signInWithPopup } from "firebase/auth";
-import { Box, Button, Card, CardBody, CardFooter, Carousel, Form, FormField,
-   Grommet, Image, TextInput } from "grommet";
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Carousel, Form,
+  FormField, Grommet, Heading, Image, TextInput } from "grommet";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCVe4J0EdfxXa1tDkx6T8XxjVf0id_2CKk",
@@ -14,10 +14,23 @@ const firebaseConfig = {
   appId: "1:184534658125:web:6f83af284a4de14b460004"
 };
 
+const theme = {
+  global: {
+    colors: {
+      brand: "#9c89ff"
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px"
+    },
+  },
+};
+
 export default function Login({ setUser }) {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const handleLogin = async(e) => {
+  const handleLogin = async (e) => {
     e.preventDefault()
     const app = initializeApp(firebaseConfig)
     const auth = getAuth(app)
@@ -26,7 +39,7 @@ export default function Login({ setUser }) {
     setUser(response.user)
   }
 
-  const handleGoogleLogin = async() => {
+  const handleGoogleLogin = async () => {
     const app = initializeApp(firebaseConfig)
     const auth = getAuth(app)
     const provider = new GoogleAuthProvider()
@@ -36,35 +49,38 @@ export default function Login({ setUser }) {
   }
 
   return (
-    <Grommet>
+    <Grommet theme={theme}>
       <Box height="medium" width="fill" overflow="hidden">
-        <Carousel fill controls={false} play={5000}>
+        <Carousel fill controls={false} play={3000}>
           <Image fit="cover" src="//images.unsplash.com/photo-1629479363534-ee1aa11b1e3c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLWxpa2VkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" />
           <Image fit="cover" src="//images.unsplash.com/photo-1617137556171-8e0832a88829?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLWxpa2VkfDI2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60" />
           <Image fit="cover" src="//images.unsplash.com/photo-1595558884516-0a2eb78964e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLWxpa2VkfDMwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60" />
-          <Image fit="cover" src="//images.unsplash.com/photo-1619844175408-c05947985e2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLWxpa2VkfDI1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60" />
+          {/* <Image fit="cover" src="//images.unsplash.com/photo-1619844175408-c05947985e2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLWxpa2VkfDI1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60" /> */}
         </Carousel>
       </Box>
-        <Card style={{margin: "auto"}} background="light-4">
+      <Form onSubmit={handleLogin}>
+        <Card background="light-4">
+          <CardHeader alignSelf="center">
+            <Heading>Welcome, Please Login</Heading>
+          </CardHeader>
           <CardBody pad="medium">
-            <Form onSubmit={handleLogin}>
-              <FormField htmlFor="email">Email:{" "}
-                <TextInput type="email" name="email"
-                  value={email} onChange= {e => setEmail(e.target.value)}
-                  placeholder="youremail@email.com" />
-              </FormField> 
-              <FormField htmlFor="password">Password:{" "}
-                <TextInput type="password" name="password"
-                  value={password} onChange= {e => setPassword(e.target.value)}
-                  placeholder="••••••" />
-              </FormField>
-              <Button primary label="Login" type="submit"/> <br/>
-            </Form>
+            <FormField htmlFor="email">Email:{" "}
+              <TextInput type="email" name="email"
+                value={email} onChange={e => setEmail(e.target.value)}
+                placeholder="youremail@email.com" />
+            </FormField>
+            <FormField htmlFor="password">Password:{" "}
+              <TextInput type="password" name="password"
+                value={password} onChange={e => setPassword(e.target.value)}
+                placeholder="••••••" />
+            </FormField>
           </CardBody>
-          <CardFooter pad="small" direction="row" gap="medium" alignSelf="center">
+          <CardFooter pad="small" direction="row" gap="xlarge" alignSelf="center">
+            <Button primary label="Login" type="submit" />
             <Button secondary label="Sign In With Google" onClick={handleGoogleLogin} />
           </CardFooter>
-        </Card>      
+        </Card>
+      </Form>
     </Grommet>
   )
 }
